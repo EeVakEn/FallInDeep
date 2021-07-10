ea44315f524f38e876607a3720f2724343f1815c
OptimalOption <vladislavdonov@inbox.ru>
2021-07-10 21:52:00 +0600

    Added effect to coins and sound

--- FallInDeep/Assets/Scripts/Relationship.cs
+++ FallInDeep/Assets/Scripts/Relationship.cs
@@ -5,43 +5,52 @@ using UnityEngine.SceneManagement;
 public class Relationship : MonoBehaviour
 {
     public int coinValue = 1;
-    public Animator anim1,anim2; 
+    public Animator anim1, anim2;
     private Animator anim;
+    public GameObject effect;
     private void Start()
     {
-        if(Public.charOn == 1){
+        if (Public.charOn == 1)
+        {
             anim = anim1;
-        }else if(Public.charOn == 2){
+        }
+        else if (Public.charOn == 2)
+        {
             anim = anim2;
         }
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag.Equals("platform")){
-            GameObject.FindWithTag("Player").GetComponent<healthbar>().fill-=1f;
-            GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().constraints=RigidbodyConstraints2D.FreezePositionX;
-            GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().constraints=RigidbodyConstraints2D.FreezeRotation;
+        if (other.gameObject.tag.Equals("platform"))
+        {
+            GameObject.FindWithTag("Player").GetComponent<healthbar>().fill -= 1f;
+            GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
+            GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
             anim.SetTrigger("isDieOnPlatform");
         }
-        if(other.gameObject.tag.Equals("health")){
-            GameObject.FindWithTag("Player").GetComponent<healthbar>().fill+=0.1f;
+        if (other.gameObject.tag.Equals("health"))
+        {
+            GameObject.FindWithTag("Player").GetComponent<healthbar>().fill += 0.1f;
             Destroy(other.gameObject);
             anim.SetTrigger("isHeal");
 
         }
-        if(other.gameObject.tag.Equals("enemy")){
-            
-            GameObject.FindWithTag("Player").GetComponent<healthbar>().fill-=0.4f;
-            
+        if (other.gameObject.tag.Equals("enemy"))
+        {
+
+            GameObject.FindWithTag("Player").GetComponent<healthbar>().fill -= 0.4f;
+
             Destroy(other.gameObject);
             anim.SetTrigger("isHit");
 
         }
-        if(other.gameObject.tag.Equals("coin")){
+        if (other.gameObject.tag.Equals("coin"))
+        {
             //+баланс
             CoinManager.instance.ChangeScore(coinValue);
+            Instantiate(effect, transform.position, Quaternion.identity);
             Destroy(other.gameObject);
 
         }
