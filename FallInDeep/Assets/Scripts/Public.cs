1d8e0b05cbcf7c95a8e2b9224f723a68d04f1772
EeVakEn <danucha129@gmail.com>
2021-07-12 17:34:31 +0600

    sdfsd

--- FallInDeep/Assets/Scripts/Public.cs
+++ FallInDeep/Assets/Scripts/Public.cs
@@ -7,7 +7,7 @@ public static class Public
     public static int score;
     public static int coins;
     public static int record;
-    public static int control=3;
+    public static int control;
     public static float musicvolume = 0f;
     public static int charOn=1;
     public static int weaponOn=1;
