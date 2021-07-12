fe891875d4d6461fd21051301ba0d68b026d9783
OptimalOption <vladislavdonov@inbox.ru>
2021-07-12 15:10:38 +0600

    Update

---
+++ FallInDeep/Assets/Scripts/Weapon2.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon2 : MonoBehaviour
+{
+
+    public Transform firePoint;
+    public Transform firePoint2;
+    public Transform firePoint3;
+    public Transform PointEffect;
+    public GameObject bullet;
+    public GameObject effect;
+    private Animator anim;
+    public AudioSource ShotSound;
+    // Update is called once per frame
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+            ShotSound.pitch = Random.Range(0.9f, 1.1f);
+            ShotSound.Play();
+            anim.SetTrigger("reload");
+            Instantiate(effect, PointEffect.position, Quaternion.identity);
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, firePoint.position, firePoint.rotation);
+        Instantiate(bullet, firePoint2.position, firePoint2.rotation);
+        Instantiate(bullet, firePoint3.position, firePoint3.rotation);
+    }
+}
